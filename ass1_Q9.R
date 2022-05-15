83a8bcf89ac4c05c2674145fc89138cedecbcaed
maithilikulkarni21 <67729523+maithilikulkarni21@users.noreply.github.com>
2022-05-16 00:10:31 +0530

    Add files via upload

---
+++ ass1_Q9.R
@@ -0,0 +1,9 @@
+data <- read.csv("C:/Users/Acer/JUPYTER/Assignments/Basic Statistics_Level 1/Q9_a.csv")
+
+skewness(data$speed)
+skewness(data$Index)
+skewness(data$dist)
+
+kurtosis(data$Index)
+kurtosis(data$speed)
+kurtosis(data$dist)
